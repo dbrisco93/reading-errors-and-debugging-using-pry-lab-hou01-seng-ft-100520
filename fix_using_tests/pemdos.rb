865e8b6af251a413872f4d7ce706d32a1725ae38
Dominique Brisco <dbrisco93@gmail.com>
2020-10-04 00:28:04 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,10 +1,8 @@
 # don't forget to add: require 'pry'
 
 def snake_it_up(string)
-  binding.pry
   if string[0] == "s"
-    binding.pry
-    10 * "s" + string
+    "s"*10 + string
   else
     string
   end
